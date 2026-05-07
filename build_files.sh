cf25e66724bd466b68dc0f97ca08ff67dfe1d09e
juangallardo19 <juangallardocsfn@gmail.com>
2026-05-07 13:44:26 -0500

    final

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-python manage.py migrate --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+python3 manage.py migrate --noinput
