a3dc0b2c0d96ed68fca1ad4ad9d1b965f51dd2a4
juangallardo19 <161619122+juangallardo19@users.noreply.github.com>
2026-05-07 13:49:00 -0500

    Add virtual environment setup to build script

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-python3 manage.py collectstatic --noinput --clear
-python3 manage.py migrate --noinput
+#!/bin/bash
+python3 -m venv /tmp/venv
+/tmp/venv/bin/pip install -r requirements.txt
+/tmp/venv/bin/python manage.py collectstatic --noinput --clear
+/tmp/venv/bin/python manage.py migrate --noinput
