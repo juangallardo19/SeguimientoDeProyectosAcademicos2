1a0c969ede58bf1fac82954d3ab382b8df2f6866
Juan Felipe <juanfelipezambrano2006@gmail.com>
2026-05-07 11:50:25 -0500

    feat: add project list and create workflow

--- Ejercicio2App/urls.py
+++ Ejercicio2App/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    path('', views.ProyectoListView.as_view(), name='proyecto-list'),
+    path('proyectos/', views.ProyectoListView.as_view(), name='proyecto-list'),
+    path('proyectos/nuevo/', views.ProyectoCreateView.as_view(), name='proyecto-create'),
 ]
