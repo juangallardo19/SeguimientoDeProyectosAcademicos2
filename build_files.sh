1c702072ca8142ed055d77e1a6b27d88e7664f00
juangallardo19 <161619122+juangallardo19@users.noreply.github.com>
2026-05-07 13:53:42 -0500

    Create superuser 'juang' if not already present
    Add command to create superuser if it doesn't exist

--- build_files.sh
+++ build_files.sh
@@ -3,3 +3,11 @@ python3 -m venv /tmp/venv
 /tmp/venv/bin/pip install -r requirements.txt
 /tmp/venv/bin/python manage.py collectstatic --noinput --clear
 /tmp/venv/bin/python manage.py migrate --noinput
+/tmp/venv/bin/python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='juang').exists():
+    User.objects.create_superuser('juang', 'juang@admin.com', '12345678')
+    print('Superuser juang creado.')
+else:
+    print('Superuser juang ya existe.')
+"
